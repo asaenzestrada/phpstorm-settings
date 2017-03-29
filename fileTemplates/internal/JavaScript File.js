431729e18bd1b9715b108b124deafdb9915d6dbc
Alex Sáenz <asaenzestrada@gmail.com>
2017-03-29 11:39:21 -0700

    PS-2016.3 <asaenz@asaenz Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, terminal.xml, ignore.xml, keymap.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, git.xml, databaseDrivers.xml, JavaScript File.js, Default.xml, laf.xml, Eclipse copy.xml, PHP File Header.php, Default _1_.xml, web-browsers.xml, vcs.xml, databaseSettings.xml, editor.xml, usageView.xml, webServers.xml, debugger.xml

---
+++ fileTemplates/internal/JavaScript File.js
@@ -0,0 +1,9 @@
+    /**
+     * SYCET by TJ ALTA TECNOLOGIA Y APLICACIONES, S. DE R.L. DE C.V. is licensed under a
+     * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+     * Based on a work at http://sycet.net and its subdomains.
+     *
+     * @link      http://gotribit.com/
+     * @copyright Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+     *
+     */
